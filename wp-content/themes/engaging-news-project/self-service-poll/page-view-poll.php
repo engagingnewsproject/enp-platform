363af32bd2d3c8e4c0ec2a475436bb09d5b637cf
Andrew Cobb <cobb.andrew@gmail.com>
2013-11-25 17:30:52 -0500

    Adding spacing.

--- wp-content/themes/engaging-news-project/self-service-poll/page-view-poll.php
+++ wp-content/themes/engaging-news-project/self-service-poll/page-view-poll.php
@@ -17,9 +17,9 @@ Template Name: View Poll
 		<h3>iframe Code</h3>
 		<div class="bootstrap">
       <p>Copy and paste this code into your target website.</p>
-	    <textarea class="form-control" rows="5"><?php echo '<iframe height="450" width="475" frameborder="0" hspace="0" src="' . get_site_url() . '/iframe-poll/?guid=' . $_GET["guid"] . '"></iframe>' ?></textarea>
+	    <div class="form-group"><textarea class="form-control" rows="5"><?php echo '<iframe height="450" width="475" frameborder="0" hspace="0" src="' . get_site_url() . '/iframe-poll/?guid=' . $_GET["guid"] . '"></iframe>' ?></textarea></div>
       <div class="clear"></div>
-	    <p><a href="configure-poll/?edit_guid=<?php echo $_GET["guid"] ?>" class="btn btn-info btn-xs active" role="button">Edit poll</a> | <a href="list-polls/?delete_guid=<?php echo $_GET["guid"] ?>" onclick="return confirm('Are you sure you want to delete this poll?')" class="btn btn-danger btn-xs active" role="button">Delete poll</a> | <a href="list-polls/" class="btn btn-primary btn-xs active" role="button">Back to polls</a></p>
+	    <div class="form-group"><p><a href="configure-poll/?edit_guid=<?php echo $_GET["guid"] ?>" class="btn btn-info btn-xs active" role="button">Edit poll</a> | <a href="list-polls/?delete_guid=<?php echo $_GET["guid"] ?>" onclick="return confirm('Are you sure you want to delete this poll?')" class="btn btn-danger btn-xs active" role="button">Delete poll</a> | <a href="list-polls/" class="btn btn-primary btn-xs active" role="button">Back to polls</a></p></div>
     </div>
     
 		<?php if ( 'on' == get_option('trim_show_pagescomments') ) comments_template('', true); ?>
