49c0e9fb0b73558a73a5d7642b81f3c9bd3d226b
Andrew Cobb <cobb.andrew@gmail.com>
2013-12-10 14:07:17 -0500

    Adding panels to view poll page.

--- wp-content/themes/engaging-news-project/self-service-quiz/page-quiz-view.php
+++ wp-content/themes/engaging-news-project/self-service-quiz/page-quiz-view.php
@@ -37,30 +37,38 @@ Template Name: View Quiz
     <h1>Quiz</h1>
     <span class="bootstrap top-edit-button"><a href="configure-quiz/?edit_guid=<?php echo $_GET["guid"] ?>" class="btn btn-info active" role="button">Edit Quiz</a></span>
     <h4>Created <?php echo $poll_created_date; ?></h4>
-    <span class="bootstrap"><hr></span>
-    <h3>Preview Quiz</h3>
-    <span class="bootstrap"><hr></span>
-    <?php get_template_part('self-service-quiz/quiz-display', 'page'); ?>
-    <div class="clear"></div>
-    <?php 
-      
-    if ( $correct_answer ) {
-    ?>
-    <p><b>Correct Answer</b>: <i><?php echo $correct_answer ?></i></p>
-    <?php } ?>
-    <span class="bootstrap"><hr></span>
-		<h3>iframe Code</h3>
-		<div class="bootstrap">
-      <?php $iframe_url = get_site_url() . '/iframe-quiz/?guid=' . $_GET["guid"]; ?>
-      <p>Copy and paste this code into your target website.  <a href="<?php echo $iframe_url ?>" target="_blank">Preview iframe</a>.</p>
-	    <div class="form-group">
-        <textarea class="form-control" id="quiz-iframe-code" rows="5"><?php echo '<iframe height="450" width="475" frameborder="0" hspace="0" src="' . $iframe_url . '"></iframe>' ?></textarea>
+    <!-- <span class="bootstrap"><hr></span> -->
+    <!-- <h3>Preview Quiz</h3>
+    <span class="bootstrap"><hr></span> -->
+    <div class="bootstrap">
+      <div class="panel panel-primary">
+        <div class="panel-heading">Preview Quiz</div>
+        <div class="panel-body">
+          <?php get_template_part('self-service-quiz/quiz-display', 'page'); ?>
+          <?php 
+          if ( $correct_answer ) {
+          ?>
+          <div class="well"><span><b>Correct Answer</b>: <i><?php echo $correct_answer ?></i></span></div>
+          <?php } ?>
+        </div>
       </div>
       <div class="clear"></div>
+      <div class="panel panel-primary">
+        <div class="panel-heading">iframe Markup</div>
+        <div class="panel-body">
+          <?php $iframe_url = get_site_url() . '/iframe-quiz/?guid=' . $_GET["guid"]; ?>
+          <p>Copy and paste this markup into your target website.  <a href="<?php echo $iframe_url ?>" target="_blank">Preview iframe</a>.</p>
+    	    <div class="form-group">
+            <textarea class="form-control" id="quiz-iframe-code" rows="5"><?php echo '<iframe height="450" width="475" frameborder="0" hspace="0" src="' . $iframe_url . '"></iframe>' ?></textarea>
+          </div>
+          <div class="clear"></div>
+        </div>
+      </div>
 	    <div class="form-group">
         <p>
           <a href="configure-quiz/?edit_guid=<?php echo $_GET["guid"] ?>" class="btn btn-info btn-xs active" role="button">Edit Quiz</a> | <a href="list-quizzes/?delete_guid=<?php echo $_GET["guid"] ?>" onclick="return confirm('Are you sure you want to delete this quiz?')" class="btn btn-danger btn-xs active" role="button">Delete Quiz</a>  | <a href="quiz-report/?guid=<?php echo $_GET["guid"] ?>" class="btn btn-primary btn-xs active" role="button">Quiz Reports</a></p>
-        <p><a href="configure-quiz" class="btn btn-info btn-xs active" role="button">New Quiz</a> | <a href="list-quizzes/" class="btn btn-primary btn-xs active" role="button">Back to Quizzes</a></p></div>
+        <p><a href="configure-quiz" class="btn btn-info btn-xs active" role="button">New Quiz</a> | <a href="list-quizzes/" class="btn btn-primary btn-xs active" role="button">Back to Quizzes</a></p>
+      </div>
     </div>
     
 		<?php if ( 'on' == get_option('trim_show_pagescomments') ) comments_template('', true); ?>
