bafa566cf3d4835946e95f23e76b4b6940f21a8c
Andrew Cobb <cobb.andrew@gmail.com>
2013-12-28 17:05:46 -0600

    Change welcome text for new users.

--- wp-content/themes/engaging-news-project/self-service-quiz/page-list-quizzes.php
+++ wp-content/themes/engaging-news-project/self-service-quiz/page-list-quizzes.php
@@ -118,7 +118,7 @@ if ( $user_ID && $_GET["delete_guid"] ) {
         else
         {
           ?>
-          <h2>No quizzes found for the current user</h2>
+          <p>Welcome!  Click <i><a href="configure-quiz/">New quiz</a></i> to get started!</p>
           <?php
         }
         ?>
