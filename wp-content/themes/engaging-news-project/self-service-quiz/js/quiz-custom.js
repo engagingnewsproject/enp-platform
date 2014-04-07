5ba05022d23fc1d6a8e476b1145a0b2bf2e8f3ea
Andrew Cobb <cobb.andrew@gmail.com>
2014-04-07 09:24:05 -0500

    Adding IE9 custom styling.

--- wp-content/themes/engaging-news-project/self-service-quiz/js/quiz-custom.js
+++ wp-content/themes/engaging-news-project/self-service-quiz/js/quiz-custom.js
@@ -1,8 +1,28 @@
 (function ($) {
+  // IE Versions 
+  var ie = (function(){
+
+      var undef,
+          v = 3,
+          div = document.createElement('div'),
+          all = div.getElementsByTagName('i');
+
+      while (
+          div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->',
+          all[0]
+      );
+
+      return v > 4 ? v : undef;
+  }());
+  
   $(function() {
     ///////////////////
     // BEGIN REPORT PAGE 
     ///////////////////
+    
+    if ( ie <= 9 ) {
+      $('body').addClass('ie9-and-lower');
+    }
     
     $('#add-my-ip').click(function(event){
       
