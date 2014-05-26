3ad0aa266499ed836c8e1d9d59a70ea67304b4d7
Brett Bowlin <brettbowlin@gmail.com>
2014-05-26 14:45:44 -0500

    Added viewport meta tag to properly define viewport width

--- wp-content/themes/yaaburnee-themes/header.php
+++ wp-content/themes/yaaburnee-themes/header.php
@@ -42,6 +42,8 @@
 
 		<meta name="description" content="<?php bloginfo('description');?>">
 
+		<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
+
 		<!-- Favicon -->
 
 		<?php 
