f8de673ac6ed62d02007840548b8684d3e3259c0
Brett Bowlin <brettbowlin@gmail.com>
2014-06-11 12:50:22 -0700

    Fixed responsive issue on 404 page

--- wp-content/themes/yaaburnee-themes/404.php
+++ wp-content/themes/yaaburnee-themes/404.php
@@ -7,10 +7,10 @@
     <!-- Primary left -->
     <div id="primary-fullwidth">
         <div class="row" id="page-404">
-            <div class="col6">
+            <div class="col-md-6">
                 <h3><?php _e("404", THEME_NAME);?> <span><?php _e(":)", THEME_NAME);?></span></h3>
             </div>
-            <div class="col6">
+            <div class="col-md-6">
                 <h5><?php _e("The page you are looking for<br>has been moved or doesn't exist anymore!", THEME_NAME);?></h5>
                 <p><?php _e("But don't worry, it can happen to the best of us - and it just happened to you!<br> You can search something else or read this text one more time.", THEME_NAME);?></p>
                 <form method="get" action="<?php echo home_url();?>">
