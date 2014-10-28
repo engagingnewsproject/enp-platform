b0925072af51a9dea03b121661e87fd00aa4ee0c
jcmacon <jcmacon@gmail.com>
2014-10-28 12:13:57 -0500

    strip slashes on questions/answers

--- wp-content/themes/yaaburnee-themes/self-service-quiz/quiz-answer.php
+++ wp-content/themes/yaaburnee-themes/self-service-quiz/quiz-answer.php
@@ -1,14 +1,14 @@
 <?php if ( $is_correct == 1) { ?>
   <h3 style="margin-top:0;"><span class="glyphicon glyphicon-check"></span> Congratulations!</h3><!-- ||KVB -->
   <div class="alert alert-success" style="width:80%;">
-    <p><b>Question:</b> <span class="quiz-question-preview"><?php echo $question_text; ?></span></p>
-    <p><b>Answer:</b> <span class="correct-answer-message" id="correct-answer"><?php echo $correct_answer_message; ?></span></p>
+    <p><b>Question:</b> <span class="quiz-question-preview"><?php echo strip_slashes($question_text); ?></span></p>
+    <p><b>Answer:</b> <span class="correct-answer-message" id="correct-answer"><?php echo  strip_slashes($correct_answer_message); ?></span></p>
   </div>
 <?php } else { ?>
   <h3 style="margin-top:0;"><span class="glyphicon glyphicon-info-sign"></span> Sorry!</h3><!-- ||KVB -->
   <div class="alert alert-info" style="width:80%;">
-    <p><b>Question:</b> <span class="quiz-question-preview"><?php echo $question_text; ?></span></p>
-    <p><b>Answer:</b><span class="incorrect-answer-message"><?php echo $incorrect_answer_message; ?></span></p>
+    <p><b>Question:</b> <span class="quiz-question-preview"><?php echo  strip_slashes($question_text); ?></span></p>
+    <p><b>Answer:</b><span class="incorrect-answer-message"><?php echo  strip_slashes($incorrect_answer_message); ?></span></p>
   </div>
 <?php } ?>
 
