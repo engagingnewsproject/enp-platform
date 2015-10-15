01b7a4ad0726791f405f0007ffece9631b994c91
Jerry <jones.jeremydavid@gmail.com>
2015-10-15 16:00:08 -0500

    removed short scripts from top of iframe page template and put them into the quiz-custom.js that was already being loaded. also removed jquery reference from iframe page template because jquery was already being loaded.

--- wp-content/themes/yaaburnee-themes/self-service-quiz/page-iframe-quiz.php
+++ wp-content/themes/yaaburnee-themes/self-service-quiz/page-iframe-quiz.php
@@ -20,20 +20,8 @@ Template Name: iframe Quiz
   <title>ENP iframe Poll</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" >
-  <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
   <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() . '/self-service-quiz/css/iframe.css'; ?>" type="text/css" media="screen" >
   <?php do_action('et_head_meta'); ?>
-  <script>
-  $(function(){
-    $('#quiz-display-form').on('submit', function(e){
-      $('.btn-primary').attr("disabled", "disabled");
-    });
-
-    $('.input-group').on('click', function(e){
-      $('.btn-primary').removeAttr("disabled");
-    });
-  });
-  </script>
 
   <?php wp_head(); ?>
 </head>
