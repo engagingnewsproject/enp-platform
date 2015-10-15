6cf9b639f5043e11bcf815b91940c96177c6961b
Jerry <jones.jeremydavid@gmail.com>
2015-10-15 09:31:20 -0500

    improving logging of user ip function

--- wp-content/themes/yaaburnee-themes/self-service-quiz/include/functions-quiz.php
+++ wp-content/themes/yaaburnee-themes/self-service-quiz/include/functions-quiz.php
@@ -446,13 +446,38 @@ function custom_wp_mail_from( $original_email_address )
 
 }
 
-function get_user_ip () {
+/*function get_user_ip () {
   $ip = $_SERVER['REMOTE_ADDR'];
   if(filter_var($ip, FILTER_VALIDATE_IP))
   {
     return $ip;
   }
   return false;
+}*/
+
+function get_user_ip() {
+  //Just get the headers if we can or else use the SERVER global
+  if ( function_exists( 'apache_request_headers' ) ) {
+    $headers = apache_request_headers();
+  } else {
+    $headers = $_SERVER;
+  }
+  //Get the forwarded IP if it exists
+  if ( array_key_exists( 'X-Forwarded-For', $headers ) && filter_var( $headers['X-Forwarded-For'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 ) ) {
+    $the_ip = $headers['X-Forwarded-For'];
+  } elseif ( array_key_exists( 'HTTP_X_FORWARDED_FOR', $headers ) && filter_var( $headers['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 )
+  ) {
+    $the_ip = $headers['HTTP_X_FORWARDED_FOR'];
+  } else {
+
+    $the_ip = filter_var( $_SERVER['REMOTE_ADDR'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 );
+  }
+  if(!empty($the_ip)){
+    return $the_ip;
+  } else {
+    return false;
+  }
+
 }
 
 function add_capability() {
