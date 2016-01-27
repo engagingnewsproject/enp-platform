de0fafba152105aa30cdcd36b43680d6e19e8bdc
Brett Bowlin <brettbowlin@gmail.com>
2016-01-27 11:06:33 -0600

    Adds link to title and remove redundant nav link to parent page

--- wp-content/themes/enp/templates/page-menu.php
+++ wp-content/themes/enp/templates/page-menu.php
@@ -1,12 +1,10 @@
-<?php 
+<?php
 	$parent = Roots\Sage\Extras\get_post_top_ancestor_id();
 ?>
 	<div class="page-menu">
-		<h5><?php echo get_the_title($parent); ?></h5>
+		<h5><a href="<?php echo get_permalink($parent); ?>"><?php echo get_the_title($parent); ?></a></h5>
 		<ul class="nav">
 	<?php
-		wp_list_pages( array( 'title_li' => false, 'include' => $parent ) );
-		global $post;
 		if ( $post->post_parent ) {
 			wp_list_pages( array( 'title_li' => false, 'child_of' => $post->post_parent ) );
 		}
