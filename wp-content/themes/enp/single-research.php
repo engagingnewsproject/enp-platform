de730b6bc72a31fb41b0192e280b23fcad095f5e
Brett Bowlin <brettbowlin@gmail.com>
2016-02-09 15:58:32 -0600

    Add new FB app id

--- wp-content/themes/enp/single-research.php
+++ wp-content/themes/enp/single-research.php
@@ -5,7 +5,7 @@
         <div class="widget widget-share-links">
     			<h5 class="widget-title">Share</h5>
     			<ul class="share-links">
-    				<li><a href="https://www.facebook.com/dialog/share?app_id=396806593831151&amp;display=popup&amp;href=<?php echo get_permalink(); ?>&amp;redirect_uri=<?php echo get_permalink(); ?>">Facebook</a></li>
+    				<li><a href="https://www.facebook.com/dialog/share?app_id=1709815112597170&amp;display=popup&amp;href=<?php echo get_permalink(); ?>&amp;redirect_uri=<?php echo get_permalink(); ?>">Facebook</a></li>
     				<li><a href="https://twitter.com/share" class="twitter-share-button"{count} data-url="<?php echo get_permalink(); ?>" data-via="engagingnews" data-related="engagingnews">Twitter</a></li>
     			</ul>
         </div>
