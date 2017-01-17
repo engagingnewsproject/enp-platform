44f78d4b6a46723a2becebf537e326affb6cf40a
Jerry <jones.jeremydavid@gmail.com>
2017-01-17 09:48:01 -0600

    updated enp-quiz

--- wp-content/plugins/enp-quiz/public/quiz-create/templates/partials/dashboard-quiz-item.php
+++ wp-content/plugins/enp-quiz/public/quiz-create/templates/partials/dashboard-quiz-item.php
@@ -3,29 +3,39 @@
 <li id="enp-dash-item--<?php echo $quiz->get_quiz_id();?>" class="enp-dash-item enp-dash-item--<?php echo $quiz->get_quiz_status();?>">
     <div class="enp-dash-item__header">
         <h3 class="enp-dash-item__title"><?php echo $this->get_quiz_dashboard_item_title($quiz);?></h3>
-        <ul id="enp-dash-item__nav--<?php echo $quiz->get_quiz_id();?>" class="enp-dash-item__nav">
-            <?php
-                $quiz_actions = $this->get_quiz_actions($quiz);
-                foreach($quiz_actions as $quiz_action) {
-                    echo '<li class="enp-dash-item__nav__item"><a href="'.$quiz_action['url'].'">'.$quiz_action['title'].'</a></li>';
-                }
-            ?>
-            <li class="enp-dash-item__nav__item">
-                <form id="enp-delete-quiz-<?php echo $quiz->get_quiz_id()?>" method="post" action="<?php echo htmlentities(ENP_QUIZ_DASHBOARD_URL.'user/'); ?>">
-                    <?php echo $nonce_input;?>
-                    <input type="hidden" class="enp-dash-item__quiz-id" name="enp_quiz[quiz_id]" value="<?php echo $quiz->get_quiz_id()?>" />
-                    <button name="enp-quiz-submit" class="enp-quiz-submit enp-dash-item__delete" value="delete-quiz">
-                        <svg class="enp-dash-item__delete__icon enp-icon enp-icon--delete">
-                          <use xlink:href="#icon-delete">
-                              <title>Delete Quiz - <?php echo $quiz->get_quiz_title();?></title>
-                          </use>
-                        </svg>
-                    </button>
-                </form>
-            </li>
-        </ul>
+        <nav>
+            <ul id="enp-dash-item__nav--<?php echo $quiz->get_quiz_id();?>" class="enp-dash-item__nav">
+                <?php
+                    $quiz_actions = $this->get_quiz_actions($quiz);
+                    foreach($quiz_actions as $quiz_action) {
+                        echo '<li class="enp-dash-item__nav__item"><a href="'.$quiz_action['url'].'">'.$quiz_action['title'].'</a></li>';
+                    }
+                ?>
+                <li class="enp-dash-item__nav__item">
+                    <form id="enp-delete-quiz-<?php echo $quiz->get_quiz_id()?>" method="post" action="<?php echo htmlentities(ENP_QUIZ_DASHBOARD_URL.'user/'); ?>">
+                        <?php echo $nonce_input;?>
+                        <input type="hidden" class="enp-dash-item__quiz-id" name="enp_quiz[quiz_id]" value="<?php echo $quiz->get_quiz_id()?>" />
+                        <button name="enp-quiz-submit" class="enp-quiz-submit enp-dash-item__delete" value="delete-quiz">
+                            <svg class="enp-dash-item__delete__icon enp-icon enp-icon--delete">
+                              <use xlink:href="#icon-delete">
+                                  <title>Delete Quiz - <?php echo $quiz->get_quiz_title();?></title>
+                              </use>
+                            </svg>
+                        </button>
+                    </form>
+                </li>
+            </ul>
+        </nav>
     </div>
     <div class="enp-dash-item__content">
+        <?php $quiz_create_date = strtotime( $quiz->get_quiz_created_at() );?>
+        <div class="enp-dash-item__meta">
+            <time class="enp-dash-item__created-at" datetime="<?php echo date( 'Y-m-dTH:i:s', $quiz_create_date );?>"><?php echo date( 'M d, Y', $quiz_create_date );?></time><?php if(current_user_can('manage_options') && isset($_GET['include']) && $_GET['include'] === 'all_users') {
+                $created_by = $quiz->get_quiz_created_by();
+                $user_info = get_userdata($created_by);
+                echo ' <span class="enp-dash-item__username">'.($user_info !== false ? "$user_info->user_email" : "user has been deleted").'</span>';
+            }?>
+        </div>
         <ul class="enp-quiz-results">
             <li class="enp-quiz-results__item enp-quiz-results__item--views">
                 <span class="enp-quiz-results__number enp-quiz-results__number--views"><?php echo $this->get_dashboard_quiz_views($quiz);?></span>
@@ -40,10 +50,5 @@
                 <div class="enp-quiz-results__label">Average</div>
             </li>
         </ul>
-        <?php if(current_user_can('manage_options') && isset($_GET['include']) && $_GET['include'] === 'all_users') {
-            $created_by = $quiz->get_quiz_created_by();
-            $user_info = get_userdata($created_by);
-            echo '<p class="enp-dash-item__username">'.($user_info !== false ? "$user_info->user_email" : "user has been deleted").'</p>';
-        }?>
     </div>
 </li>
