49720be166b53d915f840bb22b4d08249dc74846
Jerry <jones.jeremydavid@gmail.com>
2017-01-20 09:30:27 -0600

    updated quiz creator embed code again

--- wp-content/plugins/enp-quiz/public/quiz-create/templates/partials/quiz-embed-code.php
+++ wp-content/plugins/enp-quiz/public/quiz-create/templates/partials/quiz-embed-code.php
@@ -3,5 +3,5 @@
 <textarea id="enp-embed-code" class="enp-embed-code enp-publish-page__embed-code" rows="7" readonly><script type="text/javascript" src="<?php // echo ENP_QUIZ_PLUGIN_URL;
 echo 'https://engagingnewsproject.org/enp_prod/wp-content/plugins/enp-quiz/'?>public/quiz-take/js/dist/iframe-parent.min.js"></script>
 <iframe id="enp-quiz-iframe-<?php echo $quiz->get_quiz_id();?>" class="enp-quiz-iframe" src="<?php // echo ENP_QUIZ_URL
-'https://engagingnewsproject.org/quiz-embed/'.$quiz->get_quiz_id();?>" style="width: <? echo $quiz->get_quiz_width();?>; height: 500px; border: none"></iframe>
+echo 'https://engagingnewsproject.org/quiz-embed/'.$quiz->get_quiz_id();?>" style="width: <? echo $quiz->get_quiz_width();?>; height: 500px; border: none"></iframe>
 </textarea>
