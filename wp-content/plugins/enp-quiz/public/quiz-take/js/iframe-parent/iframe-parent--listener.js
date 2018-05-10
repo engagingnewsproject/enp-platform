366faa799bd119aa247dd3b3da4aac40dfae5b2a
Jerry <jones.jeremydavid@gmail.com>
2018-05-10 15:28:22 -0500

    Updated enp-quiz plugin

--- wp-content/plugins/enp-quiz/public/quiz-take/js/iframe-parent/iframe-parent--listener.js
+++ wp-content/plugins/enp-quiz/public/quiz-take/js/iframe-parent/iframe-parent--listener.js
@@ -13,7 +13,7 @@ function handleEnpIframeMessage(event) {
 
     // quit the postmessage loop if it's NOT from a trusted site (engagingnewsproject.org or our dev sites)
     // If you want to see what it matches/doesn't match, go here: http://regexr.com/3g4rc
-    if(!/https?:\/\/(?:local.quiz|quiz.dev|(?:(?:local|dev|test)\.)?(engagingnewsproject|mediaengagement)\.(?:org|dev)|(?:engagingnews|enpdev|cmengage)\.(?:staging\.)?wpengine\.com)\b/.test(event.origin)) {
+    if(!/https?:\/\/(?:quiz.dev|(?:(?:local|dev|test)\.)?(mediaengagement)\.(?:org|dev)|(?:cmengagetest|cmengage)\.(?:staging\.)?wpengine\.com)\b/.test(event.origin)) {
         return false;
     }
 
