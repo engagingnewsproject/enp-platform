4bbe407b8a94585aad5938e15764d481ad3854ac
Jerry <jones.jeremydavid@gmail.com>
2018-05-14 09:25:24 -0500

    Updated enp-quiz

--- wp-content/plugins/enp-quiz/public/quiz-create/templates/partials/quiz-create-question.php
+++ wp-content/plugins/enp-quiz/public/quiz-create/templates/partials/quiz-create-question.php
@@ -48,7 +48,7 @@
     <div class="enp-question-inner enp-answer-explanation">
         <fieldset class="enp-fieldset enp-answer-explanation__fieldset">
             <label class="enp-label enp-answer-explanation__label" for="enp-question-explanation__<?php echo $question_i;?>">Answer Explanation</label>
-            <textarea id="enp-question-explanation__<?php echo $question_i;?>" class="enp-textarea enp-answer-explanation__textarea" name="enp_question[<?php echo $question_i;?>][question_explanation]" maxlength="6120" placeholder="Your cerebellum can predict your own actions, so you're unable to 'surprise' yourself with a tickle."><?php echo $question->get_question_explanation();?></textarea>
+            <textarea id="enp-question-explanation__<?php echo $question_i;?>" class="enp-textarea enp-answer-explanation__textarea" name="enp_question[<?php echo $question_i;?>][question_explanation]" maxlength="6120" rows="5" placeholder="Your cerebellum can predict your own actions, so you're unable to 'surprise' yourself with a tickle."><?php echo $question->get_question_explanation();?></textarea>
         </fieldset>
     </div>
 </section>
