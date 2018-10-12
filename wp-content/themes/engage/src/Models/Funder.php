abc9994709be7089f2e9ee5921fde0f188a005e7
leviv <villarreallevi@gmail.com>
2018-10-12 15:35:11 -0500

    Prepare changes for about page

--- wp-content/themes/engage/src/Models/Funder.php
+++ wp-content/themes/engage/src/Models/Funder.php
@@ -5,9 +5,8 @@ class Funder extends Article {
 
 	public $featured = false;
 
-	public function __construct($postID = null)
-    {
-        parent::__construct($postID);
-        $this->featured = has_term('featured', 'funders_category', $this->ID);
-    }
+	public function __construct($postID = null){
+    parent::__construct($postID);
+    $this->featured = has_term('featured', 'funders_category', $this->ID);
+	}
 }
