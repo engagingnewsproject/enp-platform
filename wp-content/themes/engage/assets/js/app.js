59c64a59fd93497898decff5480fde3c40f56679
leviv <villarreallevi@gmail.com>
2018-10-19 14:16:37 -0500

    Get quiz creator working again and remove unneeded js code

--- wp-content/themes/engage/assets/js/app.js
+++ wp-content/themes/engage/assets/js/app.js
@@ -39,18 +39,24 @@ if(document.getElementById('orbit-balls')) {
 	})
 }
 
+
+/*
+	Code for making the timeline events appear/disappear on scroll on the quiz creator landing page
+	Currrently commented out because it's pretty inefficient and don't want to include it in this release.
+	If I find a better way to implement it 
+
 	window.onscroll = function() {updateScroll()};
 
 	function updateScroll() {
-		var startAnchor = $("#startAnchor").offset().top; // Height in px of start of scroll
-		var windowHeight = $(window).height();
+		var startAnchor = (document.getElementById("startAnchor").getBoundingClientRect().top + windowHeight); // Height in px of start of scroll
+		var windowHeight = window.scrollY;
 		var curHeight = $(document).scrollTop() + (windowHeight / 2);
 		var scrolled = 0;
 
 		// Set vars to 1 if we've scrolled past, 0 otherwise
-		var stepOneAnchor = curHeight > $("#stepOneAnchor").offset().top ? 1 : 0;
-		var stepTwoAnchor = curHeight > $("#stepTwoAnchor").offset().top ? 1 : 0;
-		var stepThreeAnchor = curHeight > $("#stepThreeAnchor").offset().top ? 1 : 0;
+		var stepOneAnchor = curHeight > (document.getElementById("stepOneAnchor").getBoundingClientRect().top + windowHeight) ? 1 : 0;
+		var stepTwoAnchor = curHeight > (document.getElementById("stepTwoAnchor").getBoundingClientRect().top + windowHeight) ? 1 : 0;
+		var stepThreeAnchor = curHeight > (document.getElementById("stepThreeAnchor").getBoundingClientRect().top + windowHeight) ? 1 : 0;
 
 		// Update the opactity of the images
 		$("#stepOneAnchor").css("opacity", stepOneAnchor);
@@ -58,8 +64,9 @@ if(document.getElementById('orbit-balls')) {
 		$("#stepThreeAnchor").css("opacity", stepThreeAnchor);
 
 		if (curHeight > startAnchor){ // Only change the height if we've scrolled past anchr
-			scrolled = ((curHeight - startAnchor));
+			scrolled = curHeight - startAnchor;
 		}
 
 		document.getElementById("myBar").style.height = scrolled + "px"; // Change the height of progress bar
 	}
+*/
