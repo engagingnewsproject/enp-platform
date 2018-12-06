b9275eeecacea549aebf64168c12563a393ff502
Chris Nunes <chrisnunes57@gmail.com>
2018-12-06 12:47:01 -0600

    Created TeamUser class, not sure how to use it yet

--- wp-content/themes/engage/single.php
+++ wp-content/themes/engage/single.php
@@ -8,20 +8,17 @@
  * @subpackage  Timber
  * @since    Timber 0.1
  */
-
 $context = Timber::get_context();
 if(is_singular('research') || is_singular('case-study')) {
 	$post = new Engage\Models\ResearchArticle();
-} 
+}
 elseif(is_singular('tribe_events')) {
 	$post = new Engage\Models\Event();
 }
 else {
 	$post = new Engage\Models\Article();
 }
-
 $context['post'] = $post;
-
 if ( post_password_required( $post->ID ) ) {
 	Timber::render( 'single-password.twig', $context );
 } else {
