64e102b2aaeef22859958f170b7ec456abed230d
sghsri <sghsri@gmail.com>
2019-01-28 14:45:45 -0600

    styling changes

--- wp-content/themes/engage/dist/js/app.js
+++ wp-content/themes/engage/dist/js/app.js
@@ -86,7 +86,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "dist/js/chunk/" + ({}[chunkId]||chunkId) + "." + {"0":"1d6afe4d9cfcfdbf6e49","1":"97dc2e0f217397f6692c"}[chunkId] + ".js";
+/******/ 		script.src = __webpack_require__.p + "dist/js/chunk/" + ({}[chunkId]||chunkId) + "." + {"0":"c680a00624859fffef35","1":"97dc2e0f217397f6692c"}[chunkId] + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
