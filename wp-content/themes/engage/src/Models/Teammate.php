02f15dc27cdddb163437cedb6096525f7dc3959b
Chris Nunes <chrisnunes57@gmail.com>
2019-03-06 15:11:32 -0600

    Changed grouping on team page to group members by position

--- wp-content/themes/engage/src/Models/Teammate.php
+++ wp-content/themes/engage/src/Models/Teammate.php
@@ -6,7 +6,8 @@ class Teammate extends Article {
 	public $name,
            $designation = false,
            $email = false,
-		   $phone = false;
+		   		 $phone = false,
+					 $terms = false;
 
 	public function __construct($postID = null)
     {
@@ -34,5 +35,12 @@ class Teammate extends Article {
         }
         return $this->phone;
     }
+
+		public function getTerms() {
+				if($this->terms === false) {
+					$this->terms = get_the_terms($this->ID, 'team_category');
+				}
+				return $this->terms;
+		}
 
 }
