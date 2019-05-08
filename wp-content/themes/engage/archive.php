ef64c4f784d3a7386a261b8a4a5950f8db644db7
sghsri <sghsri@gmail.com>
2019-05-08 15:14:07 -0500

    sorting by alphabetical and menu order

--- wp-content/themes/engage/archive.php
+++ wp-content/themes/engage/archive.php
@@ -183,12 +183,31 @@ function comparator($a, $b) {
   if($a_index == $b_index) {
     return 0;
   }
-
   return $a_index < $b_index ? -1 : 1;
 }
+
+function teamGroupComparator($a, $b) {
+    //first sort by menu order, then alphabetical
+    if($a->menu_order > $b->menu_order){
+        return 1;
+    } else if($a->menu_order < $b->menu_order){
+        return -1;
+    } else {
+        return strcmp($a->name, $b->name);
+    }
+}
+
 if(is_post_type_archive(['team']) || is_tax('team_category')) {
   // This will ensure that our columns stay sorted for our team member names.
   usort($context['archive']->filters['terms']['journalism']['terms'], "comparator");
+  //now sort the individual teamGroups
+  foreach($context['archive']['teamGroups'] as $group){
+      if(count($group) != 0){
+          usort($group["mates"],"teamGroupComparator");
+          $context['archive']['teamGroups'][$group['name']]['mates'] = $group['mates'];
+      }
+  }
+
 }
 
 Timber::render( ['archive.twig'], $context, ENGAGE_PAGE_CACHE_TIME);
