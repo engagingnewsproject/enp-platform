3f91540433b1a404e3e90ec5c374b2a43c87ec4f
Lay-ton <laytonseal@gmail.com>
2019-08-26 13:46:03 -0500

    Moved newsletter on mission page, moved context[newsletter] to Theme.php

--- wp-content/themes/engage/about.php
+++ wp-content/themes/engage/about.php
@@ -8,5 +8,4 @@
  $post = new TimberPost();
  $context['post'] = $post;
  $context['about'] = new Engage\Models\About();
- $context['newsletter'] = Timber::get_widgets('newsletter');
  Timber::render( [ 'page-about.twig' ], $context, ENGAGE_PAGE_CACHE_TIME );
