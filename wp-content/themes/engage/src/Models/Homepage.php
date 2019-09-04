7cf824438a84a895c4eee7efee4880842d85b905
sghsri <sghsri@gmail.com>
2019-09-04 16:36:24 -0500

    admin can now set a post as featured and have it show up in slider (1 per vertical), also the number of more research has been set per vertical

--- wp-content/themes/engage/src/Models/Homepage.php
+++ wp-content/themes/engage/src/Models/Homepage.php
@@ -3,6 +3,14 @@ namespace Engage\Models;
 use Engage\Managers\Queries as Queries;
 use Timber\PostQuery;
 use Timber\Post;
+use \WP_Query;
+
+
+function console_log( $data ){
+  echo '<script>';
+  echo 'console.log('. json_encode( $data ) .')';
+  echo '</script>';
+}
 
 class Homepage extends Post {
 
@@ -38,33 +46,103 @@ class Homepage extends Post {
 
         // Loop through each of the verticals
         for ($i = 0; $i < count($verticals); $i++) {
-
             $verticalName = $verticals[$i]->slug;
-
-            // Get the most recent post for that specific vertical
-            $queryResults = $this->Query->getRecentPosts([
-                'postType' => 'research',
-                'vertical' => $verticalName,
-                'postsPerPage' => 3,
-            ]);
-
-            $tempArray = array($queryResults[0]);
-            $tempArrayMoreRecent = array($queryResults[1], $queryResults[2]);
-
-            // Merge the new post with the existing ones
-            // (Maybe there's a more efficient way to do this?)
-            $this->recent = array_merge($tempArray, $this->recent);
-            $this->moreRecent = array_merge($tempArrayMoreRecent, $this->moreRecent);
+             // Get the most recent post and moreResearch posts for that specific vertical
+            $this->recent = array_merge($this->getRecentFeaturedResearch($verticalName), $this->recent);
+            $this->moreRecent = array_merge($this->getMoreRecentResearch($this->recent, $verticalName), $this->moreRecent);
         }
-				// sort the posts by their time, and then remove the oldest post.
-				usort($this->recent, function($a, $b){
-					return strtotime($b->post_date) - strtotime($a->post_date);
-				});
+
+        // sort the posts by their time
+        $this->sortByDate($this->recent);
+        $this->sortByDate($this->moreRecent);
+    }
+
+    //get the most recent featured research
+    public function getRecentFeaturedResearch($verticalName){
+        $featuredPosts = $this->queryPosts(true, $verticalName);
+        $recentFeaturedPosts = array();
+        //only show one featured research per vertical;
+        for($i = 0; $i<count($featuredPosts); $i++){
+            array_push($recentFeaturedPosts, $featuredPosts[$i]);
+            break;
+        }
+        return $recentFeaturedPosts;
+    }
+
+    //get the more research posts
+	public function getMoreRecentResearch($featuredSliderPosts, $verticalName){
+        // how many more_research_posts should be display on the home page for each vertical
+        $numFeaturedPerVertical = [
+            "journalism" => 4,
+            "media-ethics" => 2,
+            "science-communication" => 2
+        ];
+        $allRecentResearch = $this->queryPosts(false, $verticalName);
+        $moreRecentResearch = array();
+        for($i = 0; $i<count($allRecentResearch) && $numFeaturedPerVertical[$verticalName] > 0; $i++){
+            $curr = $allRecentResearch[$i];
+            // make sure not already displaying the post, and that we haven't used up the allotted num of posts
+            if($this->notInSlider($curr, $featuredSliderPosts)){
+                $numFeaturedPerVertical[$verticalName]--;
+                array_push($moreRecentResearch, $curr);
+            }
+        }
+        return $moreRecentResearch;
+	}
+
+    // check if a post with the same id as curr is not on the slider
+    public function notInSlider($curr, $featuredSliderPosts){
+        for($i = 0; $i<count($featuredSliderPosts); $i++){
+            // posts with same id are the same post
+            if($curr->id == $featuredSliderPosts[$i]->id){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // query the posts with the given arguments
+    public function queryPosts($is_featured, $verticalName){
+        $args = [
+            'postType' => 'research',
+            'vertical' => $verticalName,
+            'postsPerPage' => '100',
+        ];
+        if($is_featured){
+            // add extraQuery if want to get only posts that are marked by the admin to "show"
+            // in the featured_research custom field
+            $args['extraQuery'] = [
+                'meta_query' => [
+                    'relation' => 'OR',
+                    [
+                        'key' => 'featured_research',
+                        'value' => serialize(array('Show')),
+                        'compare' => 'LIKE'
+                    ],
+                    [
+                        'key' => 'featured_research',
+                        'value' => serialize(array('Showpost')),
+                        'compare' => 'LIKE'
+                    ]
+                ],
+            ];
+        }
+        return $this->Query->getRecentPosts($args);
+    }
+
+    // sort by the date
+    public function sortByDate($posts){
+        usort($posts, function($a, $b){
+            return strtotime($b->post_date) - strtotime($a->post_date);
+        });
     }
 
     public function setVerticals() {
         $this->verticals = $this->Query->getVerticals();
     }
+
+
+
 
 
 }
