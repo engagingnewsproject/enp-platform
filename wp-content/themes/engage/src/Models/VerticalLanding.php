a7ca7d39809b1ae2a3201bd238e836ef3920d21d
Lay-ton <laytonseal@gmail.com>
2020-03-12 15:48:50 -0500

    fix up landing page

--- wp-content/themes/engage/src/Models/VerticalLanding.php
+++ wp-content/themes/engage/src/Models/VerticalLanding.php
@@ -2,36 +2,55 @@
 namespace Engage\Models;
 use Timber\PostQuery;
 
+function console_log( $data ){
+  echo '<script>';
+  echo 'console.log('. json_encode( $data ) .')';
+  echo '</script>';
+}
+
 class VerticalLanding extends Article {
 
 	public $video = false,
-				 $directors = false;
+				 $directors = false,
+				 $menulinks = false;
 
 	  public function __construct($pid = null) {
         parent::__construct($pid);
     }
 
 		public function getVideoEmbedLink() {
-			if($this->video === false) {
-    		$this->video = get_field('video_embed_link');
-    	}
-    	return $this->video;
+				if($this->video === false) {
+	    			$this->video = get_field('video_embed_link');
+	    	}
+	    	return $this->video;
 		}
 
 		public function getDirectors() {
-      if($this->directors === false) {
-          $this->directors = new PostQuery(
-              [
-                  'post_type'=> 'team',
-                  'post_status' => 'publish',
-                  'post__in' => get_field('project_team_member', $this->ID),
-                  'orderby' => 'post__in',
-                  'order' => 'ASC',
-                  'posts_per_page' => -1
-              ],
-              'Engage\Models\Teammate'
-          );
-      }
-      return $this->directors;
+	      if($this->directors === false) {
+		        $this->directors = new PostQuery(
+		            [
+		                'post_type'=> 'team',
+		                'post_status' => 'publish',
+		                'post__in' => get_field('project_team_member', $this->ID),
+		                'orderby' => 'post__in',
+		                'order' => 'ASC',
+		                'posts_per_page' => -1
+		            ],
+		            'Engage\Models\Teammate'
+		        );
+	      }
+      	return $this->directors;
     }
+
+		public function getMenuLinks() {
+				if($this->menulinks === false) {
+						$menu_items = get_field('vertical_menu_links');
+						foreach($menu_items as $item) {
+								$split = explode(": ", $item['menu_item']);
+								$this->menulinks[$split[0]] = $split[1];
+						}
+				}
+				console_log($this->menulinks);
+				return $this->menulinks;
+		}
 }
