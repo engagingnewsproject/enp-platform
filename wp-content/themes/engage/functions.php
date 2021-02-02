a6a59a56131d101757803a3a48ba3193fff799c3
= <leoncai197@gmail.com>
2021-02-02 14:45:18 -0600

    Fixed ajax bug causing previous events not to be sorted properly

--- wp-content/themes/engage/functions.php
+++ wp-content/themes/engage/functions.php
@@ -56,7 +56,9 @@ add_filter( 'pre_get_posts', 'tribe_change_event_order', 99 );
 // When all previous events are viewable in one page, the events will
 // be sorted from most recent to oldest
 function tribe_change_event_order( $query ) {
-    if ( $query->get( 'posts_per_page' ) == -1 && tribe_is_past() ) {
+    $past_ajax = (defined( 'DOING_AJAX' ) && DOING_AJAX && $_REQUEST['tribe_event_display'] === 'past') ? true : false;
+
+    if ( $query->get( 'posts_per_page' ) == -1 && (tribe_is_past() || $past_ajax) ) {
         $query->set( 'orderby', 'date' );
         $query->set( 'order', 'ASC' );
         add_filter( 'tribe_get_events_title', 'tribe_alter_event_archive_titles', 11, 2 );
