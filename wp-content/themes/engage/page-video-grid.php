a03297879b8bdbf92952becc065c445f588cd570
luukee <luke@lukecarlhartman.com>
2021-03-26 18:30:43 -0700

    mobile check on video grid template

--- wp-content/themes/engage/page-video-grid.php
+++ wp-content/themes/engage/page-video-grid.php
@@ -10,6 +10,4 @@ $context = Timber::get_context();
 $post = new TimberPost();
 
 $context['post'] = $post;
-$cover_image_id = $post->cover_image;
-$context['cover_image'] = new Timber\Image($cover_image_id);
 Timber::render(['page-video-grid.twig'], $context, ENGAGE_PAGE_CACHE_TIME);
