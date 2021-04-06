ebf4332b7c84c361845e8c190c5dc129c5b6048b
luukee <luke@lukecarlhartman.com>
2021-04-06 10:20:18 -0700

    Revert "updated video grid to work on older browsers"
    This reverts commit 5eedd238f84a0c8f8dddd7e6fa80bc9db7bbd1bd.

--- wp-content/themes/engage/page-video-grid.php
+++ wp-content/themes/engage/page-video-grid.php
@@ -10,4 +10,5 @@ $context = Timber::get_context();
 $post = new TimberPost();
 
 $context['post'] = $post;
+$context['site_copyright_info'] = get_field('alternate_logo', 'options');
 Timber::render(['page-video-grid.twig'], $context, ENGAGE_PAGE_CACHE_TIME);
