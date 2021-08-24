c17ec054ed88c3e25b43bceeb76fe49f280f8ea4
Nick Webb <nicholas.webb@utexas.edu>
2021-08-24 16:45:23 -0500

    template with working gifs

--- wp-content/themes/engage/page-public-interest-standard.php
+++ wp-content/themes/engage/page-public-interest-standard.php
@@ -10,12 +10,21 @@ $context = Timber::get_context();
 $post = new TimberPost();
 
 $context['post'] = $post;
-$context['site_copyright_info'] = get_field('alternate_logo', 'options');
+// $context['site_copyright_info'] = get_field('alternate_logo', 'options');
 if (isset($post->gif1) && strlen($post->gif1)) {
   $post->gif1 = new Timber\Image($post->gif1);
 }
-if (isset($post->title_gif2) && strlen($post->title_gif2)) {
-  $post->title_gif2 = new Timber\Image($post->title_gif2);
+if (isset($post->gif2) && strlen($post->gif2)) {
+  $post->gif2 = new Timber\Image($post->gif2);
+}
+if (isset($post->gif3) && strlen($post->gif3)) {
+  $post->gif3 = new Timber\Image($post->gif3);
+}
+if (isset($post->gif4) && strlen($post->gif4)) {
+  $post->gif4 = new Timber\Image($post->gif4);
+}
+if (isset($post->gif5) && strlen($post->gif5)) {
+  $post->gif5 = new Timber\Image($post->gif5);
 }
 Timber::render(['page-public-interest-standard.twig'], $context, ENGAGE_PAGE_CACHE_TIME);
 
