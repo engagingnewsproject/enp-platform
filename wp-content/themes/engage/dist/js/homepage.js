88ba3fdb8932885e9e87c876ce27d2484d6471ea
Luke Carl Hartman <luke@lukecarlhartman.com>
2022-10-25 15:34:46 -0700

    update flickity slider module & manual select ACF

--- wp-content/themes/engage/dist/js/homepage.js
+++ wp-content/themes/engage/dist/js/homepage.js
@@ -1,16 +1,15 @@
-jQuery(document).ready(function($) {
-
+jQuery(document).ready(function ($) {
 	let maxChars = 42;
 
 	// Loop through each of the titles
-	$('.home-intro .tile__title').each(function() {
+	$(".home-intro .tile__title").each(function () {
 		let title = $(this).html(); // Grab the title text
 
 		// If the title is too large
 		if (title.length > maxChars) {
 			// Find the end of the next word after 40 characters
 			let spaceSearch = title.substring(maxChars, title.length);
-			let spaceIndex = spaceSearch.indexOf(' ');
+			let spaceIndex = spaceSearch.indexOf(" ");
 			// Make sure we're not currently on the last word And that the word isn't too big
 			if (spaceIndex != -1 && spaceIndex < 10) {
 				title = title.substring(0, spaceIndex + maxChars);
@@ -23,20 +22,16 @@ jQuery(document).ready(function($) {
 	});
 });
 
-
 // initiate Flickity
 // https://flickity.metafizzy.co/#initialize-with-vanilla-javascript
 
-var elem = document.querySelector('.carousel-main');
-var flkty = new Flickity( elem, {
-  // options
+var elem = document.querySelector(".carousel-main");
+var flkty = new Flickity(elem, {
+	// options
 	wrapAround: true,
-	fullscreen: true,
+	adaptiveHeight: true,
 	imagesLoaded: true,
 	lazyLoad: 1,
-	percentPosition: false,
-  contain: true
+	percentPosition: true,
+	contain: true,
 });
-
-
-
