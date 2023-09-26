ca1b7421a8969af6b28e0685fe1e8f728a125396
Luke Carl Hartman <luke@lukecarlhartman.com>
2023-09-25 17:03:11 -0700

    research page new header layout

--- wp-content/themes/engage/src/Models/ResearchArticle.php
+++ wp-content/themes/engage/src/Models/ResearchArticle.php
@@ -3,38 +3,46 @@ namespace Engage\Models;
 use Timber\PostQuery;
 
 class ResearchArticle extends Article {
-
-	public $report = false,
-		   $summary = false,
-			 $video = false,
-       $researchers = false;
-
-	public function __construct($pid = null)
+    
+    public $report = false,
+    $summary = false,
+    $summary_content = false,
+    $video = false,
+    $researchers = false;
+    
+    public function __construct($pid = null)
     {
         parent::__construct($pid);
     }
-
+    
     public function getReport() {
-    	if($this->report === false) {
-    		$this->report = get_field('report_here');
-    	}
-    	return $this->report;
+        if($this->report === false) {
+            $this->report = get_field('report_here');
+        }
+        return $this->report;
     }
-
+    
     public function getSummary() {
-    	if($this->summary === false) {
-    		$this->summary = get_field('summary_research_');
-    	}
-    	return $this->summary;
+        if($this->summary === false) {
+            $this->summary = get_field('summary_research_');
+        }
+        return $this->summary;
     }
-
-		public function getVideoEmbedLink() {
-			if($this->video === false) {
-    		$this->video = get_field('video_embed_link');
-    	}
-    	return $this->video;
-		}
-
+    
+    public function getSummaryContent() {
+        if($this->summary_content === false) {
+            $this->summary_content = get_field('summary_content');
+        }
+        return $this->summary_content;
+    }
+    
+    public function getVideoEmbedLink() {
+        if($this->video === false) {
+            $this->video = get_field('video_embed_link');
+        }
+        return $this->video;
+    }
+    
     public function getResearchers() {
         if($this->researchers === false) {
             $this->researchers = new PostQuery(
