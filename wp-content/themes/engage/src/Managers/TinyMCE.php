78bbeb19687639d0cdde4f988e90ac68699e118b
Luke Carl Hartman <luke@lukecarlhartman.com>
2023-10-11 13:04:28 -0700

    valid_children' key is $init array fix

--- wp-content/themes/engage/src/Managers/TinyMCE.php
+++ wp-content/themes/engage/src/Managers/TinyMCE.php
@@ -29,7 +29,14 @@ class TinyMCE {
         } else {
             $init['extended_valid_elements'] = $ext;
         }
-            $init['valid_children'] .= "+figure[div|span|canvas|h1|h2|h3|h4|h5|h6],+div[canvas],+a[em|strong|small|mark|abbr|dfn|i|b|s|u|code|var|samp|kbd|sup|sub|q|cite|span|bdo|bdi|br|wbr|ins|del|img|embed|object|iframe|map|area|noscript|ruby|video|audio|input|textarea|select|button|label|output|datalist|keygen|progress|command|canvas|time|meter|p|hr|pre|ul|ol|dl|div|h1|h2|h3|h4|h5|h6|hgroup|address|blockquote|section|nav|article|aside|header|footer|figure|table|f|m|fieldset|menu|details|style|link],+body[style|link|figure]";
+        
+        // Initialize 'valid_children' if it doesn't exist
+        if (!isset($init['valid_children'])) {
+            $init['valid_children'] = '';
+        }
+        
+        // Add to 'valid_children'
+        $init['valid_children'] .= "+figure[div|span|canvas|h1|h2|h3|h4|h5|h6],+div[canvas],+a[em|strong|small|mark|abbr|dfn|i|b|s|u|code|var|samp|kbd|sup|sub|q|cite|span|bdo|bdi|br|wbr|ins|del|img|embed|object|iframe|map|area|noscript|ruby|video|audio|input|textarea|select|button|label|output|datalist|keygen|progress|command|canvas|time|meter|p|hr|pre|ul|ol|dl|div|h1|h2|h3|h4|h5|h6|hgroup|address|blockquote|section|nav|article|aside|header|footer|figure|table|f|m|fieldset|menu|details|style|link],+body[style|link|figure]";
 
         // Super important: return $init!
         return $init;
