72a5b1f7d5fd5bf219492aa17f5a9d8d7166e038
Luke Carl Hartman <luke@lukecarlhartman.com>
2023-11-30 09:38:34 -0800

    minor singe.php fixes

--- wp-content/themes/engage-2-x/single.php
+++ wp-content/themes/engage-2-x/single.php
@@ -9,13 +9,12 @@
  * @since    Timber 0.1
  */
 
-$context	= Timber::context();
-$post			= $context['post'];
+$context			= Timber::context();
+$timber_post	= Timber::get_post();
+$context['post'] = $timber_post;
 
 if ( post_password_required( $timber_post->ID ) ) {
 	Timber::render( 'single-password.twig', $context );
 } else {
-	# Set additional team members up, parse string into list of users
-	$context['post']->additional_team_members_list = explode(', ', $post->additional_team_members);
 	Timber::render( array( 'single-' . $timber_post->ID . '.twig', 'single-' . $timber_post->post_type . '.twig', 'single-' . $timber_post->slug . '.twig', 'single.twig' ), $context );
 }
