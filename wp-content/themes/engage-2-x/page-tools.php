ab9e5d197dd253154ec4cd8fe9bcf85f696632d2
Luke Carl Hartman <luke@lukecarlhartman.com>
2024-02-09 11:50:17 -0800

    tools page fix

---
+++ wp-content/themes/engage-2-x/page-tools.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Template Name: Tools
+ * Description: A Page Template for Tools page
+ */
+
+$context 	= Timber::context();
+$post 		= $context['post'];
+
+Timber::render([ 'page-tools.twig' ], $context, ENGAGE_PAGE_CACHE_TIME);
