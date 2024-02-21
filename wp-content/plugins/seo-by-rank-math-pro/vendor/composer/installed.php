f398534a4c56293785335b19567619dcd3a8fff1
Luke Carl Hartman <luke@lukecarlhartman.com>
2024-02-21 07:34:21 -0800

    rank math pro plugin update

---
+++ wp-content/plugins/seo-by-rank-math-pro/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rankmath/seo-by-rank-math-pro',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => '660ea268084617c08ed017974df0dad7496e623f',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'rankmath/seo-by-rank-math-pro' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => '660ea268084617c08ed017974df0dad7496e623f',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
