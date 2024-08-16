82dcd6c10b418763774ec90ae68035baf929ef0c
Luke Carl Hartman <luke@lukecarlhartman.com>
2024-08-16 12:15:37 -0700

    the_post_template explain

--- wp-content/themes/engage-2-x/page-annual-report.php
+++ wp-content/themes/engage-2-x/page-annual-report.php
@@ -8,6 +8,8 @@
 // Code to display Page goes here...
 $context 	= Timber::context();
 $post 		= $context['post'];
-$context['the_post_template'] = 'annual-report'; // Set the template name in the context
+$context['the_post_template'] = 'annual-report'; // Set the template name in the context 
+// - see base.twig where 'the_post_template' is used to conditionally change the <main>
+// element's wrapper_padding class and brand_bar visibility
 
 Timber::render([ 'page-annual-report.twig' ], $context, ENGAGE_PAGE_CACHE_TIME);
