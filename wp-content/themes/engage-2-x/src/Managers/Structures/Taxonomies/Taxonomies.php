38186fa20cde93e63b30d295cca04a98abf4ab64
Luke C Hartman <luke@lukecarlhartman.com>
2025-03-05 16:01:48 -0800

    remove verticals from wp-admin

--- wp-content/themes/engage-2-x/src/Managers/Structures/Taxonomies/Taxonomies.php
+++ wp-content/themes/engage-2-x/src/Managers/Structures/Taxonomies/Taxonomies.php
@@ -2,73 +2,86 @@
 /*
  * Registers taxonomies
  */
+
 namespace Engage\Managers\Structures\Taxonomies;
 
-class Taxonomies {
-		// An array to store the taxonomies that need to be registered
-		protected $taxonomies = [];
+class Taxonomies
+{
+	// An array to store the taxonomies that need to be registered
+	protected $taxonomies = [];
 
-		/**
-		 * Constructor to initialize the class with a list of taxonomies.
-		 *
-		 * @param array $taxonomies An array of taxonomy method names to be registered.
-		 */
-		public function __construct($taxonomies) {
-				$this->taxonomies = $taxonomies;
+	/**
+	 * Constructor to initialize the class with a list of taxonomies.
+	 *
+	 * @param array $taxonomies An array of taxonomy method names to be registered.
+	 */
+	public function __construct($taxonomies)
+	{
+		$this->taxonomies = $taxonomies;
+	}
+
+	/**
+	 * Method to run the registration process for each taxonomy.
+	 *
+	 * Iterates over the list of taxonomies provided during class instantiation
+	 * and calls each taxonomy's registration method.
+	 */
+	public function run()
+	{
+		foreach ($this->taxonomies as $taxonomy) {
+			$this->$taxonomy();
 		}
+	}
 
-		/**
-		 * Method to run the registration process for each taxonomy.
-		 *
-		 * Iterates over the list of taxonomies provided during class instantiation
-		 * and calls each taxonomy's registration method.
-		 */
-		public function run() {
-				foreach($this->taxonomies as $taxonomy) {
-						$this->$taxonomy();
-				}
-		}
+	/**
+	 * Method to register the 'Verticals' taxonomy.
+	 *
+	 * This method defines the 'Verticals' taxonomy, including its labels, 
+	 * settings, and the post types it applies to. The taxonomy is hidden from
+	 * the WordPress admin interface but remains functional in the backend.
+	 */
+	public function Verticals()
+	{
+		// Labels for the 'Verticals' taxonomy
+		$labels = array(
+			'name'              => _x('Verticals', 'taxonomy general name'),
+			'singular_name'     => _x('Vertical', 'taxonomy singular name'),
+			'search_items'      => __('Search Verticals'),
+			'all_items'         => __('All Verticals'),
+			'parent_item'       => __('Parent Vertical'),
+			'parent_item_colon' => __('Parent Vertical:'),
+			'edit_item'         => __('Edit Vertical'),
+			'update_item'       => __('Update Vertical'),
+			'add_new_item'      => __('Add New Vertical'),
+			'new_item_name'     => __('New Vertical Name'),
+			'menu_name'         => __('Verticals'),
+		);
 
-		/**
-		 * Method to register the 'Verticals' taxonomy.
-		 *
-		 * This method defines the 'Verticals' taxonomy, including its labels, 
-		 * settings, and the post types it applies to. It registers the taxonomy
-		 * with WordPress using the specified settings.
-		 */
-		public function Verticals() {
-				// Labels for the 'Verticals' taxonomy
-				$labels = array(
-						'name'              => _x( 'Verticals', 'taxonomy general name' ),
-						'singular_name'     => _x( 'Vertical', 'taxonomy singular name' ),
-						'search_items'      => __( 'Search Verticals' ),
-						'all_items'         => __( 'All Verticals' ),
-						'parent_item'       => __( 'Parent Vertical' ),
-						'parent_item_colon' => __( 'Parent Vertical:' ),
-						'edit_item'         => __( 'Edit Vertical' ),
-						'update_item'       => __( 'Update Vertical' ),
-						'add_new_item'      => __( 'Add New Vertical' ),
-						'new_item_name'     => __( 'New Vertical Name' ),
-						'menu_name'         => __( 'Verticals' ),
-				);
+		// Arguments for the 'Verticals' taxonomy
+		$args = array(
+			'hierarchical'      => true,
+			'labels'            => $labels,
+			'show_ui'           => false, // Hide from admin UI
+			'show_admin_column' => false, // Hide from admin columns
+			'show_in_menu'      => false, // Hide from admin menu
+			'query_var'         => true,
+			'has_archive'       => true,
+			'rewrite'           => ['slug' => 'vertical'],
+		);
 
-				// Arguments for the 'Verticals' taxonomy
-				$args = array(
-						'hierarchical'      => true,
-						'labels'            => $labels,
-						'show_ui'           => true,
-						'show_admin_column' => true,
-						'query_var'         => true,
-						'has_archive'       => true,
-						'rewrite'           => [ 'slug' => 'vertical' ],
-				);
+		// Post types to which the 'Verticals' taxonomy will be registered
+		$postTypes = [
+			'post',
+			'page',
+			'research',
+			'funders',
+			'announcement',
+			'blogs',
+			'tribe_events',
+			'board'
+		];
 
-				// Post types to which the 'Verticals' taxonomy will be registered
-				$postTypes = [
-						'post', 'page', 'research', 'funders', 'announcement', 'blogs', 'tribe_events', 'board'
-				];
-
-				// Register the 'Verticals' taxonomy for the specified post types with the provided arguments
-				register_taxonomy( 'verticals', $postTypes, $args );
-		}
+		// Register the 'Verticals' taxonomy for the specified post types with the provided arguments
+		register_taxonomy('verticals', $postTypes, $args);
+	}
 }
