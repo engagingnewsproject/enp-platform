130dca2cdcbe8bc5846747eed5562816e2279f63
Luke C Hartman <luke@lukecarlhartman.com>
2025-03-21 16:46:19 -0700

    archive-press.php is the same as the archive-publications.php file

--- wp-content/themes/engage-2-x/archive-press.php
+++ wp-content/themes/engage-2-x/archive-press.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying Press Archive pages.
+ * The template for displaying Archive pages.
  *
  * Used to display archive-type pages if nothing more specific matches a query.
  * For example, puts together date-based pages if no date.php file exists.
@@ -12,15 +12,15 @@ namespace App;
 
 use Timber\Timber;
 
-$templates = [ 'templates/archive-press.twig', 'templates/index.twig' ];
+$templates = [ 'templates/archive.twig', 'templates/index.twig' ];
 
-$title = 'Press Archive';
+$title = 'Archive';
 if ( is_day() ) {
-	$title = 'Press Archive: ' . get_the_date( 'D M Y' );
+	$title = 'Archive: ' . get_the_date( 'D M Y' );
 } elseif ( is_month() ) {
-	$title = 'Press Archive: ' . get_the_date( 'M Y' );
+	$title = 'Archive: ' . get_the_date( 'M Y' );
 } elseif ( is_year() ) {
-	$title = 'Press Archive: ' . get_the_date( 'Y' );
+	$title = 'Archive: ' . get_the_date( 'Y' );
 } elseif ( is_tag() ) {
 	$title = single_tag_title( '', false );
 } elseif ( is_category() ) {
@@ -30,20 +30,9 @@ if ( is_day() ) {
 	array_unshift( $templates, 'templates/archive-' . get_post_type() . '.twig' );
 }
 
-// Get posts
-$posts = Timber::get_posts();
-
 $context = Timber::context(
 	[
 		'title' => $title,
-		'posts' => $posts,
-		'debug' => [
-			'post_type' => get_post_type(),
-			'query_vars' => $wp_query->query_vars,
-			'found_posts' => $wp_query->found_posts,
-			'post_count' => $wp_query->post_count,
-			'posts_count' => count($posts)
-		]
 	]
 );
 
