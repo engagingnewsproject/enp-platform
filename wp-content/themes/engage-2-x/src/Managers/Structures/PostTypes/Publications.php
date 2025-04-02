41c2e2b9b66f49f27500ce088305eac08d627b37
Luke C Hartman <luke@lukecarlhartman.com>
2025-04-02 12:51:09 -0700

    publication tags do not show in wp-admin

--- wp-content/themes/engage-2-x/src/Managers/Structures/PostTypes/Publications.php
+++ wp-content/themes/engage-2-x/src/Managers/Structures/PostTypes/Publications.php
@@ -168,8 +168,8 @@ class Publications
 		$args = array(
 			'hierarchical' => false,
 			'labels' => $labels,
-			'show_ui' => true,
-			'show_admin_column' => true,
+			'show_ui' => false,
+			'show_admin_column' => false,
 			'show_in_menu' => false,
 			'update_count_callback' => '_update_post_term_count',
 			'query_var' => true,
