5ca22cf023c3d575938254ed34f96f56918eff3f
Luke C Hartman <luke@lukecarlhartman.com>
2025-04-02 14:47:21 -0700

    Press page template - use ACF Taxonomy field

--- wp-content/themes/engage-2-x/page-press.php
+++ wp-content/themes/engage-2-x/page-press.php
@@ -4,20 +4,37 @@
  * Description: A Page Template for Press
  */
 
+use Timber\Timber;
+
 $context = Timber::context();
 $post    = $context['post'];
 
-function console_log($output, $with_script_tags = true)
-{
-	$js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) .');';
-	if ($with_script_tags) {
-		$js_code = '<script>' . $js_code . '</script>';
-	}
-	echo $js_code;
+// Get the term objects from ACF field
+$terms = get_field('posts'); // This will return term objects since that's selected in ACF
+
+// Get posts from those terms if terms exist
+if ($terms && !empty($terms)) {
+    // Extract term IDs from term objects
+    $term_ids = array_map(function($term) {
+        return $term->term_id;
+    }, $terms);
+
+    $press_posts = get_posts([
+        'post_type' => 'press',
+        'numberposts' => -1,
+        'tax_query' => [
+            [
+                'taxonomy' => 'press-categories',
+                'field'    => 'term_id',
+                'terms'    => $term_ids
+            ]
+        ]
+    ]);
+
+    // Convert to Timber posts
+    $context['press_posts'] = Timber::get_posts($press_posts);
+} else {
+    $context['press_posts'] = [];
 }
 
-# we need to start generating some rows from effectively what is a csv file
-console_log($context);
-console_log($post);
-
-Timber::render([ 'page-press.twig' ], $context, ENGAGE_PAGE_CACHE_TIME);
+Timber::render(['page-press.twig'], $context, ENGAGE_PAGE_CACHE_TIME);
