ecbe84ed799dcdccb9fc495f83ebf9f7770c77b3
Luke C Hartman <luke@lukecarlhartman.com>
2025-05-02 10:01:08 -0700

    rm verticals from wp-admin screens

--- wp-content/themes/engage-2-x/functions.php
+++ wp-content/themes/engage-2-x/functions.php
@@ -17,7 +17,7 @@ use Engage\Managers\Login;
 use Engage\Managers\Permalinks;
 use Engage\Managers\Queries;
 use Engage\Managers\Structures\PostTypes\PostTypes;
-use Engage\Managers\Structures\Taxonomies\Taxonomies;
+// use Engage\Managers\Structures\Taxonomies\Taxonomies;
 use Engage\Managers\Theme;
 use Engage\Managers\TinyMCE;
 // Models
@@ -108,7 +108,7 @@ add_action('after_setup_theme', function () {
 		new Permalinks(),
 		new Queries(),
 		new PostTypes(['Research', 'Blogs', 'Announcement', 'Team', 'Funders', 'Board', 'Publications', 'Press']),
-		new Taxonomies(['Verticals']), // temporary keep true for initial testing
+		// new Taxonomies(['Verticals']), // temporary keep true for initial testing
 		new TinyMCE()
 	];
 
