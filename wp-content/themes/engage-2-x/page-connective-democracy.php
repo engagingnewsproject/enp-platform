a639528f4925b8bd9951dd2f5e8796c36dc7cf1e
Luke C Hartman <luke@lukecarlhartman.com>
2025-05-08 16:11:25 -0700

    Enhance 'Connective Democracy' page with research tiles functionality
    - Added a new term slug for 'bridging-divides' to filter research posts.
    - Updated the Timber query to include a tax query for 'research-categories' and 'blogs-category'.
    - Modified the Twig template to conditionally display the research tiles section based on the new configuration.
    - Removed deprecated fields from the ACF JSON configuration related to research tiles.

--- wp-content/themes/engage-2-x/page-connective-democracy.php
+++ wp-content/themes/engage-2-x/page-connective-democracy.php
@@ -13,6 +13,8 @@ $newsroom_resource_posts = get_field('newsroom_resource_posts');
 $context['newsroom_resource_posts'] = $newsroom_resource_posts;
 // END newsroom resource posts
 
+$term_slug = 'bridging-divides'; // or get dynamically
+
 $context['research'] = Timber::get_posts([
 	'post_type' => [
 		'research', 
@@ -20,6 +22,19 @@ $context['research'] = Timber::get_posts([
 	], 
 	'posts_per_page' => -1, 
 	'orderby' => 'date', 
-	'order' => 'DESC'],
-);
+	'order' => 'DESC',
+	'tax_query' => [
+		'relation' => 'OR',
+		[
+			'taxonomy' => 'research-categories',
+			'field'    => 'slug',
+			'terms'    => $term_slug,
+		],
+		[
+			'taxonomy' => 'blogs-category',
+			'field'    => 'slug',
+			'terms'    => $term_slug,
+		],
+	],
+]);
 Timber::render(['page-connective-democracy.twig'], $context, ENGAGE_PAGE_CACHE_TIME); 
