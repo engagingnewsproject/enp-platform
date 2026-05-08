30ccd38f153e9c4b384058520b24e481c24a5aaa
Luke C Hartman <luke@lukecarlhartman.com>
2026-05-08 10:35:07 -0700

    plugin update
    Update Jetpack to version 15.8, introducing enhancements such as a native WP Admin page for the Activity Log, new MCP settings admin page, and improved compatibility with WordPress 7.0. Bug fixes include resolving issues with the AI admin page slug and ensuring proper functionality of forms and subscriptions. Additionally, deprecated functions have been updated to use new classes for better performance and maintainability.

--- wp-content/plugins/ninja-forms/build/form-block.asset.php
+++ wp-content/plugins/ninja-forms/build/form-block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '55ae824f8dff51d3790e');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8d748caed6da42a5fe44');
